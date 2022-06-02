576dc23fb987f4c1f08dbe6a84067c9c3a09674b
Ashutosh5426 <ashutoshgupta8840@gmail.com>
2022-06-03 00:50:54 +0530

    assignment

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,17 +1,17 @@
 const printDate = function() {
-    let currentDate = new Date()
-    console.log(currentDate)    
+  let currentDate = new Date()
+  console.log(currentDate)    
 }
 
 const printMonth = function() {
-    let currentdate = new Date()
-    let currentMonth = currentdate.getMonth() + 1
-    console.log('The current month is '+currentMonth)
+  let currentdate = new Date()
+  let currentMonth = currentdate.getMonth() + 1
+  console.log('The current month is '+currentMonth)
 }
 
 const getBatchInfo = function() {
-  let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
-  console.log(batchInformation)
+let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
+console.log(batchInformation)
 }
 
 module.exports.printDate = printDate
