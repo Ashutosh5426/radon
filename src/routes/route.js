c64bd845bc3f1a847745686fdc7d3f8f0beeefee
Ashutosh5426 <ashutoshgupta8840@gmail.com>
2022-06-05 17:06:41 +0530

    assignment mongo-session1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,44 +1,9 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const BookController= require("../controllers/bookController");
 
-let players = [
-  {
-    name: "manish",
-    dob: "1/1/1995",
-    gender: "male",
-    city: "jalandhar",
-    sports: ["swimming"],
-  },
-  {
-    name: "gopal",
-    dob: "1/09/1995",
-    gender: "male",
-    city: "delhi",
-    sports: ["soccer"],
-  },
-  {
-    name: "lokesh",
-    dob: "1/1/1990",
-    gender: "male",
-    city: "mumbai",
-    sports: ["soccer"],
-  },
-];
+router.post("/createBook", BookController.createBook  );
 
-router.post("/players", function (req, res) {
-    let data = req.body;
-    let isSame = false;
-  players.forEach(player => {
-      if(player.name == data.name){
-        isSame = true;
-      }
-
-  });
-  if(!isSame){
-      players.push(data);
-  }
-console.log(data);
-  res.send(  { data: players , status: true }  );
-});
+router.get("/getBooksData", BookController.getBooksData  );
 
 module.exports = router;
