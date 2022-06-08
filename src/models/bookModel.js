f863cd8b8e75ba03e4435c3ceade39a210bedb2d
Ashutosh5426 <ashutoshgupta8840@gmail.com>
2022-06-08 23:22:48 +0530

    mongo-session 3 assignment solution

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
   name: String,
-  author_id: String,
+  author_id: {
+    type: String,
+    required: true
+  },
   price: Number,
   ratings: String
 }, {timestamps: true});
