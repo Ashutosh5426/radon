f38f6f3bdec5a53118c0c0b4d5364b9d0f638d86
Ashutosh5426 <ashutoshgupta8840@gmail.com>
2022-06-08 23:20:12 +0530

    mongo-session 3 assignment solution

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const authorSchema = new mongoose.Schema({
-  author_id: String,
+  author_id: {
+    type: String,
+    required: true
+  },
   author_name: String,
   age: String,
   address: String
